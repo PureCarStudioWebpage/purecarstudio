c3500d6af3c7bd57c80163a83309a91aed7651a3
PureCarStudioWebpage <124194642+PureCarStudioWebpage@users.noreply.github.com>
2023-02-02 18:43:51 +0100

    feat/add variadble to eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,4 @@
+// eslint-disable-next-line no-undef
 module.exports = {
     "env": {
         "browser": true,
@@ -6,7 +7,8 @@ module.exports = {
     "extends": [
         "eslint:recommended",
         "plugin:react/recommended",
-        "plugin:@typescript-eslint/recommended"
+        "plugin:@typescript-eslint/recommended",
+        "plugin:react/jsx-runtime"
     ],
     "overrides": [
     ],
